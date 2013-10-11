8b8396fca023e2b2f2d689a61a3496cf44d4d2ad
shinyclef <shinyclef@hotmail.com>
2013-10-12 02:08:07 +1100

    Changed behaviour of unexpected disconnects and added a reconnect button. Also enabled use of enter key from username/password fields to login.

--- src/com/hotmail/shinyclef/rolydplus/FrameLogin.java
+++ src/com/hotmail/shinyclef/rolydplus/FrameLogin.java
@@ -3,10 +3,7 @@ package com.hotmail.shinyclef.rolydplus;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 /**
  * Author: Shinyclef
@@ -17,27 +14,23 @@ import java.awt.event.WindowEvent;
 
 public class FrameLogin extends JFrame
 {
-    private static final int ATTEMPTS = 5;
     private int attemptsRemaining;
     private boolean firstAttempt = true;
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JLabel feedbackLabel;
     private JButton loginButton;
+    private JButton reconnectButton;
 
     /* Constructor. */
     public FrameLogin()
     {
-        attemptsRemaining = ATTEMPTS;
         initializeUI();
     }
 
     /* The method responsible for initializing the frame with all of its components and settings. */
     public final void initializeUI()
     {
-        //the listener
-        ActionListener listener = new ButtonListener();
-
         //basic frame attributes
         setTitle("RolyDPlus Login");
         setSize(350, 280);
@@ -45,9 +38,9 @@ public class FrameLogin extends JFrame
         setResizable(false);
         addWindowListener(new WindowAdapter() {
             @Override
-            public void windowClosing(WindowEvent we) {
+            public void windowClosing(WindowEvent we)
+            {
                 RolyDPlus.initializeExit(0);
-                //setVisible(false);
             }
         });
 
@@ -72,20 +65,21 @@ public class FrameLogin extends JFrame
         passwordField = new JPasswordField(14);
         loginButton = new JButton("Login");
         JButton quitButton = new JButton("Quit");
+        reconnectButton = new JButton("Reconnect");
 
         //component formatting
         Dimension labelDimension = new Dimension(65, 30);
         Dimension buttonDimension = new Dimension(80, 30);
-
         titleLabel.setAlignmentX(CENTER_ALIGNMENT);
         feedbackLabel.setAlignmentX(CENTER_ALIGNMENT);
         feedbackLabel.setFont(new Font("Dialog", Font.PLAIN, 12));
         feedbackLabel.setVisible(false);
-
         usernameLabel.setPreferredSize(labelDimension);
         passwordLabel.setPreferredSize(labelDimension);
         loginButton.setPreferredSize(buttonDimension);
         quitButton.setPreferredSize(buttonDimension);
+        reconnectButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        reconnectButton.setVisible(false);
 
         //add the components to the panels
         boxPanel.add(titleLabel);
@@ -96,8 +90,9 @@ public class FrameLogin extends JFrame
         //boxPanel.add(Box.createVerticalStrut(0));
         boxPanel.add(feedbackLabel);
         boxPanel.add(Box.createVerticalStrut(15));
-
         boxPanel.add(buttonsPanel);
+        boxPanel.add(Box.createVerticalStrut(5));
+        boxPanel.add(reconnectButton);
 
         usernamePanel.add(usernameLabel);
         usernamePanel.add(usernameField);
@@ -106,41 +101,86 @@ public class FrameLogin extends JFrame
         buttonsPanel.add(loginButton);
         buttonsPanel.add(quitButton);
 
-        //add the listener to the buttons
-        loginButton.addActionListener(listener);
-        quitButton.addActionListener(listener);
+        //the listeners
+        MyActionListener myActionListener = new MyActionListener();
+        MyKeyListener myKeyListener = new MyKeyListener();
+
+        //add the listeners to the components
+        loginButton.addActionListener(myActionListener);
+        quitButton.addActionListener(myActionListener);
+        reconnectButton.addActionListener(myActionListener);
+        usernameField.addKeyListener(myKeyListener);
+        passwordField.addKeyListener(myKeyListener);
     }
 
     /* The action listen for the buttons. */
-    class ButtonListener implements ActionListener
+    class MyActionListener implements ActionListener
     {
         public void actionPerformed(ActionEvent ev)
         {
-            if (ev.getActionCommand().equals("Login"))
+            String buttonName = ev.getActionCommand();
+            if (buttonName.equals("Login"))
             {
-                String usernameInput = usernameField.getText();
-                char[] passwordInput = passwordField.getPassword();
-
-                //check for null/empty
-                if (usernameInput.equals("") || (passwordInput.length == 0))
-                {
-                    incompleteFieldsFeedback();
-                    return;
-                }
-
-                //set account details assuming success
-                RolyDPlus.setUsername(usernameInput);
-                RolyDPlus.setPassword(new String(passwordInput));
-
-                //send login request then wait
-                NetProtocolHelper.attemptLogin(usernameInput, new String(passwordInput));
-                attemptingLoginFeedback();
+                login();
             }
-            else
+            else if (buttonName.equals("Quit"))
             {
                 RolyDPlus.initializeExit(0);
             }
+            else if(buttonName.equals("Reconnect"))
+            {
+                reconnect();
+            }
         }
+    }
+
+    class MyKeyListener implements KeyListener
+    {
+        @Override
+        public void keyTyped(KeyEvent e)
+        {
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e)
+        {
+            if (e.getKeyCode() == 10) //10 is 'enter' key
+            {
+                login();
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e)
+        {
+        }
+    }
+
+    private void login()
+    {
+        String usernameInput = usernameField.getText();
+        char[] passwordInput = passwordField.getPassword();
+
+        //check for null/empty
+        if (usernameInput.equals("") || (passwordInput.length == 0))
+        {
+            incompleteFieldsFeedback();
+            return;
+        }
+
+        //set account details assuming success
+        RolyDPlus.setUsername(usernameInput);
+        RolyDPlus.setPassword(new String(passwordInput));
+
+        //send login request then wait
+        NetProtocolHelper.attemptLogin(usernameInput, new String(passwordInput));
+        attemptingLoginFeedback();
+    }
+
+    private void reconnect()
+    {
+        attemptingReconnectFeedback();
+        RolyDPlus.reconnect(1);
     }
 
     public void unsuccessfulLoginReply(String reason)
@@ -220,13 +260,35 @@ public class FrameLogin extends JFrame
         feedbackLabel.setVisible(true);
     }
 
+    private void attemptingReconnectFeedback()
+    {
+        feedbackLabel.setForeground(Color.YELLOW);
+        feedbackLabel.setText("<html><center>Attempting to reconnect to server...</center></html>");
+        feedbackLabel.setVisible(true);
+    }
+
+    public void reconnectSuccessfulFeedback()
+    {
+        feedbackLabel.setForeground(Color.GREEN);
+        feedbackLabel.setText("<html><center>Successfully reconnected to server.</center></html>");
+        feedbackLabel.setVisible(true);
+    }
+
+    public void reconnectUnsuccessfulFeedback()
+    {
+        feedbackLabel.setForeground(Color.RED);
+        feedbackLabel.setText("<html><center>Could not connect to server.</center></html>");
+        feedbackLabel.setVisible(true);
+    }
+
     public void enableControls()
     {
         usernameField.setEnabled(true);
         passwordField.setEnabled(true);
         loginButton.setEnabled(true);
+        reconnectButton.setVisible(false);
         feedbackLabel.setForeground(Color.BLUE);
-        feedbackLabel.setText("Server has restart. You can now login.");
+        feedbackLabel.setText("Connection established. You can now log in.");
     }
 
     public void disableControls()
@@ -234,8 +296,9 @@ public class FrameLogin extends JFrame
         usernameField.setEnabled(false);
         passwordField.setEnabled(false);
         loginButton.setEnabled(false);
+        reconnectButton.setVisible(true);
         feedbackLabel.setForeground(Color.BLUE);
-        feedbackLabel.setText("Server has shut down. Waiting for restart...");
+        feedbackLabel.setText("Lost connection with server.");
         feedbackLabel.setVisible(true);
 
     }
